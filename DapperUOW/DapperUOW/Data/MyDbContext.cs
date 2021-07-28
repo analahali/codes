8bbd7f87d2c19b2f2a1b37539861ffd48251fc5c
aalbzour <aalbzour@ABOZOUR>
2021-07-28 14:41:34 +0300

    DAPPER UOW

---
+++ DapperUOW/DapperUOW/Data/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperUOW.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options)
+           : base(options)
+        {
+        }
+    }
+}
